17ba3be2be3159a667410b283392553373d49aac
marina.shilo <marina.shilo@degree53.com>
2022-11-29 01:45:03 +0300

    added delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,29 @@
+import * as fsPromises from 'fs/promises';
+
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+
+const PATH = '/files/fileToRemove.txt';
+
 const remove = async () => {
-    // Write your code here 
+    return await fsPromises.stat(`${_dirname}${PATH}`)
+        .then((stat) => {
+            if (stat.isFile()) {
+                return fsPromises.rm(`${_dirname}${PATH}`);
+            }
+
+            throw new Error('FS operation failed');
+        })
+        .catch((error) => {
+            if (error.code === 'ENOENT') {
+                throw new Error('FS operation failed');
+            }
+
+            console.error(error);
+        });
 };
 
 await remove();
