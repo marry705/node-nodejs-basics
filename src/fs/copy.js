b2afb1bf629bc8f224d99fa36555d575e4a819ee
marina.shilo <marina.shilo@degree53.com>
2022-11-29 01:04:08 +0300

    added copy ans create functions

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,47 @@
+import * as fsPromises from 'fs/promises';
+import * as fs from 'fs';
+
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+
+const PATH = '/files';
+const COPY_PATH = '/files_copy';
+
 const copy = async () => {
-    // Write your code here 
+    fs.stat(`${_dirname}${COPY_PATH}`, async (error, stat) => {
+        if (stat?.isDirectory()) {
+            throw new Error('FS operation failed');
+        }
+
+        if (error?.code === 'ENOENT') {
+            await fsPromises.stat(`${_dirname}${PATH}`)
+                .then((stat) => {
+                    if (stat.isDirectory()) {
+                        return fsPromises.mkdir(`${_dirname}${COPY_PATH}`);
+                    }
+
+                    throw new Error('path error');
+                })
+                .then(() => fsPromises.readdir(`${_dirname}${PATH}`))
+                .then((files) => Promise.all(
+                    files.map((file) => {
+                        const source = join(`${_dirname}${PATH}`, file);
+                        const destination = join(`${_dirname}${COPY_PATH}`, file);
+                        return fsPromises.copyFile(source, destination);
+                    })
+                ))
+                .catch((error) => {
+                    if (error.code === 'ENOENT') {
+                        throw new Error('FS operation failed');
+                    }
+
+                    console.log(error);
+                })
+        }
+    });
 };
 
 copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import * as fsPromises from 'fs/promises';
+
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+
+const PATH = '/files/fresh.txt';
+const TEXT = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+    return await fsPromises.stat(`${_dirname}${PATH}`)
+        .then((stat) => {
+            if (stat.isFile()) {
+                throw new Error('FS operation failed');
+            }
+        })
+        .catch((error) => {
+            if (error.code === 'ENOENT') {
+                return fsPromises.writeFile(`${_dirname}${PATH}`, TEXT);
+            }
+
+            console.error(error);
+        });
 };
 
 await create();
