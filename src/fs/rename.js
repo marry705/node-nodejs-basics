4e77fe4c89a4e1672ee54720c8fac4bff82d3c4b
marina.shilo <marina.shilo@degree53.com>
2022-11-29 01:30:06 +0300

    added rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,42 @@
+import * as fsPromises from 'fs/promises';
+
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+
+const PATH = '/files/';
+const OLD_NAME = 'wrongFilename.txt';
+const NEW_NAME = 'properFilename.md';
+
 const rename = async () => {
-    // Write your code here 
+    return await fsPromises.stat(`${_dirname}${PATH}${NEW_NAME}`)
+        .then((stat) => {
+                if (stat.isFile()) {
+                    throw new Error('FS operation failed');
+                }
+            }, (error) => {
+                if (error.code === 'ENOENT') {
+                    return fsPromises.stat(`${_dirname}${PATH}${OLD_NAME}`);
+                }
+
+                throw new Error('FS operation failed');
+            })
+        .then((stat) => {
+            if (stat.isFile()) {
+                return fsPromises.rename(`${_dirname}${PATH}${OLD_NAME}`, `${_dirname}${PATH}${NEW_NAME}`);
+            }
+
+            throw new Error('FS operation failed');
+        })
+        .catch((error) => {
+            if (error.code === 'ENOENT') {
+                throw new Error('FS operation failed');
+            }
+
+            console.error(error);
+        })
 };
 
 await rename();
