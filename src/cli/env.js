f35cfeb8b303167d9bb41739e2cc7563a33a8e45
marina.shilo <marina.shilo@degree53.com>
2022-11-29 23:53:52 +0300

    feat: added env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+import { env } from 'process';
+
+const PREFIX = 'RSS_';
+
 const parseEnv = () => {
-    // Write your code here 
+    const rssEnvironment = Object.entries(env)
+        .filter(environment => environment[0].includes(PREFIX))
+        .map((environment) => environment.join('='))
+        .join('; ')
+
+    console.log(rssEnvironment);
 };
 
 parseEnv();
