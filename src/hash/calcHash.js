aff11ca8ad4a73dcead1cbdc9cfe3d7cfcbba0c3
marina.shilo <marina.shilo@degree53.com>
2022-11-30 13:43:41 +0300

    feat: added calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import * as fsPromises from 'fs/promises';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const { createHash } = await import('crypto');
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+
+const PATH = '/files/fileToCalculateHashFor.txt';
+
 const calculateHash = async () => {
-    // Write your code here 
+    return await fsPromises.access(`${_dirname}${PATH}`, fsPromises.constants.R_OK)
+        .then(() => fsPromises.readFile(`${_dirname}${PATH}`))
+        .then((inputData) => {
+            const hash = createHash('sha256').update(inputData).digest('hex');
+
+            console.log(hash);
+        })
+        .catch((error) => console.error(error))
 };
 
 await calculateHash();
