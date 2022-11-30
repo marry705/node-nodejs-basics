b2c39ec9e66915f07448314c9f113d833765f6d9
marina.shilo <marina.shilo@degree53.com>
2022-11-30 23:25:51 +0300

    feat: added read and write stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import * as fs from 'fs';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { stdout } from 'process';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+
+const PATH = '/files/fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(`${_dirname}${PATH}`);
+    readStream.pipe(stdout);
+    readStream.on('end', () => {
+        stdout.end('\n');
+    });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import * as fs from 'fs';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { stdin } from 'process';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+
+const PATH = '/files/fileToWrite.txt';
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = fs.createWriteStream(`${_dirname}${PATH}`);
+    stdin.pipe(writeStream);
 };
 
 await write();
